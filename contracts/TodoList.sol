734403cbcbacc17d9cb6f94703c54fdf65770b2a
carloshernandez85 <45855952+carloshernandez85@users.noreply.github.com>
2019-08-07 13:11:23 -0600

    Add files via upload
    Initial commit via site

---
+++ contracts/TodoList.sol
@@ -0,0 +1,29 @@
+pragma solidity >0.4.0 <0.6.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("check out my baller DApp");
+    }
+    
+    
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
+
+
+
+
+}
